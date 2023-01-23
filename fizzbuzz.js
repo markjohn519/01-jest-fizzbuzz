9a2ca17df34f50038103bca55b3a21e341e4f99b
markjohn519 <markjohnmonton@gmail.com>
2023-01-23 11:15:22 +0800

    added error handling

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,7 +1,11 @@
 export default function fizzbuzz(number) {
-  if (!Number.isInteger(number) || number <= 0) return 'error'
-  if (number % 3 === 0 && number % 5 === 0) return 'fizzbuzz'
-  if (number % 3 === 0) return 'fizz'
-  if (number % 5 === 0) return 'buzz'
-  return ''
+  try {
+    if (!Number.isInteger(number) || number <= 0) throw new Error('error')
+    if (number % 3 === 0 && number % 5 === 0) return 'fizzbuzz'
+    if (number % 3 === 0) return 'fizz'
+    if (number % 5 === 0) return 'buzz'
+    return ''
+  } catch (error) {
+    return error.message
+  }
 }
