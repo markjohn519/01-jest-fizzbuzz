ccbbb327841daf7a88ab3dfe975051454e9b3eef
markjohn519 <markjohnmonton@gmail.com>
2023-01-23 11:12:32 +0800

    added the check if isInteger

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,12 +1,7 @@
-export default function fizzbuzz(postiveInteger) {
-  if (postiveInteger % 3 === 0 && postiveInteger % 5 === 0) {
-    return 'fizzbuzz'
-  }
-  if (postiveInteger % 3 === 0) {
-    return 'fizz'
-  }
-  if (postiveInteger % 5 === 0) {
-    return 'buzz'
-  }
+export default function fizzbuzz(number) {
+  if (!Number.isInteger(number) || number <= 0) return 'error'
+  if (number % 3 === 0 && number % 5 === 0) return 'fizzbuzz'
+  if (number % 3 === 0) return 'fizz'
+  if (number % 5 === 0) return 'buzz'
   return ''
 }
