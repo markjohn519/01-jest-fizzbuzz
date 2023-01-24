a076d51bbc7b8f7041d6aaf79d07bb304f5f3799
markjohn519 <markjohnmonton@gmail.com>
2023-01-24 09:15:24 +0800

    remove try catch

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,11 +1,15 @@
 export default function fizzbuzz(number) {
-  try {
-    if (!Number.isInteger(number) || number <= 0) throw new Error('error')
-    if (number % 3 === 0 && number % 5 === 0) return 'fizzbuzz'
-    if (number % 3 === 0) return 'fizz'
-    if (number % 5 === 0) return 'buzz'
-    return ''
-  } catch (error) {
-    return error.message
+  if (!Number.isInteger(number) || number <= 0) {
+    throw new Error('error')
   }
+  if (number % 3 === 0 && number % 5 === 0) {
+    return 'fizzbuzz'
+  }
+  if (number % 3 === 0) {
+    return 'fizz'
+  }
+  if (number % 5 === 0) {
+    return 'buzz'
+  }
+  return ''
 }
